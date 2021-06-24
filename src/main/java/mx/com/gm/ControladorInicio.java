f4d3f9fb85c1ad03212f3bbf144b512f435eeb9a
Sonia Castro <soniacastromartel@gmail.com>
2021-06-24 20:13:17 +0100

    commit 1.1

--- src/main/java/mx/com/gm/ControladorInicio.java
+++ src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,5 +1,6 @@
 package mx.com.gm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Sonia Castro (soniacastromartel@gmail.com)
  */
 @RestController
+@Slf4j
 public class ControladorInicio {
     
     @GetMapping("/")
     public String inicio(){
+	log.info("Ejecutando..");
 	return"Hola Mundo Spring";
     }
     
