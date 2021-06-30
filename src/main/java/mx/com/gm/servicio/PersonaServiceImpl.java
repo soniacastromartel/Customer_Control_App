aa3fdfad818694334e31acccdd4b5565b12676eb
Sonia Castro <soniacastromartel@gmail.com>
2021-06-30 11:32:21 +0100

    commit 4.17 implementando capa de servicio

---
+++ src/main/java/mx/com/gm/servicio/PersonaServiceImpl.java
@@ -0,0 +1,46 @@
+package mx.com.gm.servicio;
+
+import java.util.List;
+import mx.com.gm.dao.PersonaDAO;
+import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Sonia Castro (soniacastromartel@gmail.com)
+ */
+@Service
+public class PersonaServiceImpl implements PersonaService{
+    @Autowired
+    private PersonaDAO personaDAO;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Persona> listarPersonas() {
+	return (List<Persona>) personaDAO.findAll();
+	
+    }
+
+    @Override
+    @Transactional
+    public void guardar(Persona persona) {
+	personaDAO.save(persona);
+	
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Persona persona) {
+	personaDAO.delete(persona);
+	
+    }
+
+    @Override
+    @Transactional (readOnly = true)
+    public Persona encontrarPersona(Persona persona) {
+	return personaDAO.findById(persona.getIdPersona()).orElse(null);
+    }
+    
+}
--- src/main/java/mx/com/gm/web/ControladorInicio.java
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -2,7 +2,7 @@ package mx.com.gm.web;
 
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import mx.com.gm.dao.PersonaDAO;
+import mx.com.gm.servicio.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControladorInicio {
     
  @Autowired //inyectamos la interface PersonaDAO
- private PersonaDAO personaDAO;
+ private PersonaService personaService;
     
     @GetMapping("/")
     public String inicio(Model model){
-	List personas= (List) personaDAO.findAll();
+	List personas= (List) personaService.listarPersonas();
 	
 	log.info("Ejecutando..");
 	model.addAttribute("personas", personas);
