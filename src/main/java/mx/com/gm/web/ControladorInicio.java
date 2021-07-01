703656499415f20f1a9f79823e7818ba62573cac
Sonia Castro <soniacastromartel@gmail.com>
2021-07-01 11:53:43 +0100

    commit 5.18 implementando CRUD(editar)

--- src/main/java/mx/com/gm/web/ControladorInicio.java
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -39,5 +39,12 @@ public class ControladorInicio {
 	return "redirect:/";
     }
     
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+	persona= personaService.encontrarPersona(persona);
+	model.addAttribute("persona", persona);
+	return  "modificar";
+    }
+    
     
 }
