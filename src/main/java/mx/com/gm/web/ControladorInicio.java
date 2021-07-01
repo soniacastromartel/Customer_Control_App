f835f6a39c2d8aa19cc2f742cd95d8fb1676d93a
Sonia Castro <soniacastromartel@gmail.com>
2021-07-01 11:34:48 +0100

    commit 5.18 implementando CRUD

--- src/main/java/mx/com/gm/web/ControladorInicio.java
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -2,11 +2,13 @@ package mx.com.gm.web;
 
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
 import mx.com.gm.servicio.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -27,5 +29,15 @@ public class ControladorInicio {
 	model.addAttribute("personas", personas);
 	return"index";
     }
+    @GetMapping("/agregar")
+    public  String agregar(Persona persona){
+	return "modificar";
+    }
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+	personaService.guardar(persona);
+	return "redirect:/";
+    }
+    
     
 }
