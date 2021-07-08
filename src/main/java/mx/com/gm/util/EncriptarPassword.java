3a9fa5037b1cea7c81deb97db172e37a625230c3
Sonia Castro <soniacastromartel@gmail.com>
2021-07-08 08:21:31 +0100

    commit 10.30 password encrypting with Springframework

---
+++ src/main/java/mx/com/gm/util/EncriptarPassword.java
@@ -0,0 +1,22 @@
+package mx.com.gm.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author Sonia Castro (soniacastromartel@gmail.com)
+ */
+public class EncriptarPassword {
+
+    public static void main(String[] args) {
+	String password = "123";
+	System.out.println("password:"+ password);
+	System.out.println("password encriptado: "+encriptarPassword(password));
+    }
+
+    public static String encriptarPassword(String password) {
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	return encoder.encode(password);
+    }
+
+}
